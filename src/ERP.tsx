dfaa6951179287b5b9126cf0e38f753098181cff
Vamsi-Krishna-Gottumukkala <gvamsikrishna.0420@gmail.com>
2025-05-22 15:47:40 +0530

    added navigation to formlist

--- src/ERP.tsx
+++ src/ERP.tsx
@@ -1,7 +1,26 @@
-import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../App';
 import React from 'react';
 
-const ERP = () => {
+type LoginScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'ERP'
+>;
+
+type Props = {
+  navigation: LoginScreenNavigationProp;
+};
+
+const ERP: React.FC<Props> = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.head}>
@@ -14,41 +33,44 @@ const ERP = () => {
         <Image
           source={require('../assets/translate.png')}
           style={styles.image}
+          resizeMode="contain"
         />
       </View>
-      <View style={styles.block}>
-      <View style={styles.top}>
-        <View style={styles.box}>
-          <Text>ERP 1</Text>
-          <Text>15 Froms</Text>
-        </View>
-        <View style={styles.box}>
-          <Text>ERP 2</Text>
-          <Text>15 Froms</Text>
-        </View>
-      </View>
 
-      <View style={styles.top}>
-        <View style={styles.box}>
-          <Text>ERP 3</Text>
-          <Text>15 Froms</Text>
+      <ScrollView contentContainerStyle={styles.block}>
+        <View style={styles.top}>
+          <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('FormsList')}>
+              <Text>ERP 1</Text>
+              <Text>15 Forms</Text>
+          </TouchableOpacity>
+          <View style={styles.box}>
+            <Text>ERP 2</Text>
+            <Text>15 Forms</Text>
+          </View>
         </View>
-        <View style={styles.box}>
-          <Text>ERP 4</Text>
-          <Text>15 Froms</Text>
+
+        <View style={styles.top}>
+          <View style={styles.box}>
+            <Text>ERP 3</Text>
+            <Text>15 Forms</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>ERP 4</Text>
+            <Text>15 Forms</Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.top}>
-        <View style={styles.box}>
-          <Text>ERP 5</Text>
-          <Text>15 Froms</Text>
+
+        <View style={styles.top}>
+          <View style={styles.box}>
+            <Text>ERP 5</Text>
+            <Text>15 Forms</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>ERP 6</Text>
+            <Text>15 Forms</Text>
+          </View>
         </View>
-        <View style={styles.box}>
-          <Text>ERP 6</Text>
-          <Text>15 Froms</Text>
-        </View>
-      </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -57,49 +79,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    width:"100%",
-    paddingTop:80,
+    width: '100%',
+    paddingTop: 80,
   },
   head: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width:'85%'
+    width: '85%',
+    marginBottom: 20,
   },
   heading: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 1,
   },
   subtext: {
     fontSize: 14,
-    textAlign: 'center',
     color: '#999999',
   },
   image: {
-    height: 20,
-    color: '#ffffff',
+    height: 30,
+    width: 30,
   },
-  block:{
-    alignItems:'center',
-    margin:10,
-    width:'100%',
+  block: {
+    alignItems: 'center',
+    margin: 10,
+    width: '100%',
   },
-  top:{
-    flexDirection:'row',
-    width:'95%',
-    justifyContent:'center',
+  top: {
+    flexDirection: 'row',
+    width: '95%',
+    justifyContent: 'center',
   },
-  box:{
-    width:'45%',
-    minHeight:'20%',
-    borderRadius:20,
-    borderColor:'#000',
-    borderWidth:1,
-    alignItems:'center',
-    justifyContent:'center',
-    margin:8,
-  }
+  box: {
+    width: '45%',
+    minHeight: 100,
+    borderRadius: 20,
+    borderColor: '#000',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 8,
+  },
 });
 
 export default ERP;
